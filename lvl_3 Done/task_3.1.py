8440b894853e0d3ff81c99c0c9c4901e39076a54
Максим Дорофеев <Mallius@spark-mail.ru>
2023-04-17 11:56:53 +0300

    Выполнил задание уровень 3

--- lvl_3 Done/task_3.1.py
+++ lvl_3 Done/task_3.1.py
@@ -1,11 +1,11 @@
-# Задача 3.1.
+# # Задача 3.1.
 # Создайте класс матрицы (или таблицы).
 # Требования к классу:
 #   - каждая колонка является числом от 1 до n (n любое число, которые вы поставите!)
 #   - в каждой ячейке содержится либо число, либо None
 #   - доступы следующие методы матрицы:
-#       * принимать новые значения, 
-#       * заменять существующие значения, 
+#       * принимать новые значения,
+#       * заменять существующие значения,
 #       * выводить число строк и колонок.
 
 # Пример матрицы 10 на 10 из единиц:
@@ -25,3 +25,29 @@
 #   - отображать в таблице/матрице название колонки не обязательно!
 #   - проявите фантазию :)
 
+class Matrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.table = [[None for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def set_value(self, row, col, val):
+        self.table[row][col] = val
+
+    def update_value(self, row, col, val):
+        if row >= self.rows or col >= self.cols:
+            raise ValueError("Index out of range")
+        self.table[row][col] = val
+
+    def num_rows(self):
+        return self.rows
+
+    def num_cols(self):
+        return self.cols
+
+
+matrix = Matrix(3, 4)
+matrix.set_value(0, 2, 5)
+matrix.update_value(1, 1, 10)
+print(matrix.num_rows())
+print(matrix.num_cols())
